94d069c3b369ada90763395e9f17d30b15c63034
Jordan Mannfeld <jordanmannfeld@gmail.com>
2019-06-23 21:56:24 -0400

    updating landing page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Starter - Forty V2",
-    author: "Hunter Chang",
-    description: "A Gatsby.js V2 Starter based on Forty by HTML5 UP"
+    title: 'Zealous Tech Indy',
+    author: 'Zealous Tech',
+    description: 'A place to learn more about Zealous Tech and get in contact.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -19,6 +19,6 @@ module.exports = {
       },
     },
     'gatsby-plugin-sass',
-    'gatsby-plugin-offline'
+    'gatsby-plugin-offline',
   ],
 }
